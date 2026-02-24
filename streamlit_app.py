53c3ab32349bbbff0b0fc6cb7dfd0f461db7873c
Marlonn Locatelli <marlonn@gmail.com>
2026-02-24 17:14:08 -0300

    🐛 fix: indentation

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,41 +35,41 @@ if uploaded_file is not None:
   # st.write("Dados")
   # st.dataframe(data)
 
-# data = pd.read_csv('data.csv', header=2, usecols=[5, 6, 7, 13, 17], dtype={5: manchester_dtype, 6: protocolo_dtype})
-# data = pd.read_csv('data.csv', header=2, usecols=[5, 6, 7, 13, 17], nrows=100, dtype={6: manchester_dtype})
-# Seleciona apenas as colunas necessárias
-# data = data.iloc[:, [5, 6, 7, 13, 17]]
+  # data = pd.read_csv('data.csv', header=2, usecols=[5, 6, 7, 13, 17], dtype={5: manchester_dtype, 6: protocolo_dtype})
+  # data = pd.read_csv('data.csv', header=2, usecols=[5, 6, 7, 13, 17], nrows=100, dtype={6: manchester_dtype})
+  # Seleciona apenas as colunas necessárias
+  # data = data.iloc[:, [5, 6, 7, 13, 17]]
 
-# Converte tempos em segundos
-# data['TA Coleta'] = pd.to_timedelta(data.iloc[:, 3]).dt.total_seconds()
-data['TA Coleta segundos'] = pd.to_timedelta(data['TA Coleta']).dt.total_seconds()
-data['TAT LAB segundos'] = pd.to_timedelta(data['TAT LAB']).dt.total_seconds()
+  # Converte tempos em segundos
+  # data['TA Coleta'] = pd.to_timedelta(data.iloc[:, 3]).dt.total_seconds()
+  data['TA Coleta segundos'] = pd.to_timedelta(data['TA Coleta']).dt.total_seconds()
+  data['TAT LAB segundos'] = pd.to_timedelta(data['TAT LAB']).dt.total_seconds()
 
-# print(data)
+  # print(data)
 
-# Filtra todas linhas com Manchester == URGENTE
-urgencias_df = data[data['Manchester'] == 'URGENTE']
+  # Filtra todas linhas com Manchester == URGENTE
+  urgencias_df = data[data['Manchester'] == 'URGENTE']
 
-# print(len(uregencias))
+  # print(len(uregencias))
 
-urgencias_20 = data[data['TAT LAB segundos'] <= 1200.0]
-# print(urgencias_20)
+  urgencias_20 = data[data['TAT LAB segundos'] <= 1200.0]
+  # print(urgencias_20)
 
-sepse_df = data[(data['Protocolo'] == 'SEPSE') & (data['Exame'] == 'LACTATO')]
+  sepse_df = data[(data['Protocolo'] == 'SEPSE') & (data['Exame'] == 'LACTATO')]
 
 
 
-sepse_tab, card_tab, avc_tab, emergencia_tab, murgente_tab, urgente_tab, purgente_tab =st.tabs(['SEPSE', 'DOR TORÁCICA', 'AVC', 'EMERGÊNCIA', 'MUITO URGENTE', 'URGENTE', 'POUCO URGENTE'])
+  sepse_tab, card_tab, avc_tab, emergencia_tab, murgente_tab, urgente_tab, purgente_tab =st.tabs(['SEPSE', 'DOR TORÁCICA', 'AVC', 'EMERGÊNCIA', 'MUITO URGENTE', 'URGENTE', 'POUCO URGENTE'])
 
-with sepse_tab:
-  sepse_prazo = sepse_df[sepse_df['TAT LAB segundos'] < 2100] # 2100s = 35min
+  with sepse_tab:
+    sepse_prazo = sepse_df[sepse_df['TAT LAB segundos'] < 2100] # 2100s = 35min
 
-  st.markdown(f'''
-              Tempos de entrega de **lactato** em **protocolo de sepse**.  
-              Total de registros: **{len(sepse_df)}**.  
-              Entregas dentro do prazo (35 min): **{len(sepse_prazo)}**
-              ''')
-  st.data_editor(sepse_df.iloc[:, :5])
+    st.markdown(f'''
+                Tempos de entrega de **lactato** em **protocolo de sepse**.  
+                Total de registros: **{len(sepse_df)}**.  
+                Entregas dentro do prazo (35 min): **{len(sepse_prazo)}**
+                ''')
+    st.data_editor(sepse_df.iloc[:, :5])
 
-with emergencia_tab:
-  st.data_editor(urgencias_20)
+  with emergencia_tab:
+    st.data_editor(urgencias_20)
