1e6c6dccae7e77ee16d0aafccd90c230b2e62370
Marlonn Locatelli <marlonn@gmail.com>
2026-03-03 15:43:31 -0300

    adicionado interação do DataFrame nos protocolos

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import pandas as pd
 from pandas.api.types import CategoricalDtype
 
-EXAMES_1 = ["GASO", "GASOV", "HT", "HB", "PLA", "NA", "K", "CAIO"]
-EXAMES_2 = ["H", "TAP", "KPTT", "TRP", "PCRQ", "LACTATO", "AMI"]
-EXAMES_3 = ["AUR", "ALB", "BIL", "CA", "CPK", "CKMB", "CL", "FERRO", "FAL", "LDH", "FOSFORO", "G", "LIPASE", "MAG", "PT", "GGT", "TGO", "TGP", "U", "FIB", "BHCG", "EAS"]
+import streamlit as st
 
 manchester_dtype = CategoricalDtype(
   categories=[
@@ -17,58 +15,115 @@ manchester_dtype = CategoricalDtype(
 protocolo_dtype = CategoricalDtype(
   categories=[
     "SEPSE",
-    "Janela AVC"
-    "Dor torácica"
+    "Janela AVC",
+    "Dor Torácica"
   ]
 )
 
-data = pd.read_csv('data.csv', header=2, usecols=[5, 6, 7, 13, 17], dtype={5: manchester_dtype, 6: protocolo_dtype})
-# data = pd.read_csv('data.csv', header=2, usecols=[5, 6, 7, 13, 17], nrows=100, dtype={6: manchester_dtype})
-# Seleciona apenas as colunas necessárias
-# data = data.iloc[:, [5, 6, 7, 13, 17]]
+MESES = {
+    1: "Janeiro", 2: "Fevereiro", 3: "Março",
+    4: "Abril", 5: "Maio", 6: "Junho",
+    7: "Julho", 8: "Agosto", 9: "Setembro",
+    10: "Outubro", 11: "Novembro", 12: "Dezembro"
+}
 
-# Converte tempos em segundos
-# data['TA Coleta'] = pd.to_timedelta(data.iloc[:, 3]).dt.total_seconds()
-data['TA Coleta segundos'] = pd.to_timedelta(data['TA Coleta']).dt.total_seconds()
-data['TAT LAB segundos'] = pd.to_timedelta(data['TAT LAB']).dt.total_seconds()
+METAS = {
+  'sepse': [35],
+  'dor torácica': [60],
+  'janela avc': [45],
+  'emergência': [20, 45, 80],
+  'muito urgente': [30, 60, 100],
+  'urgente': [50, 70, 120],
+  'pouco urgente': [60, 100, 130],
+  'não urgente': [80, 120, 150]
+}
 
-# print(data)
+def mostrar_protocolo_tab(tab, df, session_key, protocolo, exame, meta):
+    """
+    Exibe uma aba de protocolo com métricas de tempo.
+    """
 
-# Filtra todas linhas com Manchester == URGENTE
-urgencias_df = data[data['Manchester'] == 'URGENTE']
+    with tab:
+        st.markdown(f'##### Protocolo de *{protocolo}*: `{exame}`')
 
-# print(len(uregencias))
+        if session_key not in st.session_state:
+            st.session_state[session_key] = df.copy()
 
-urgencias_20 = data[data['TAT LAB segundos'] <= 1200.0]
-# print(urgencias_20)
+        metric_container = st.container()
 
-sepse_df = data[(data['Protocolo'] == 'SEPSE') & (data['Exame'] == 'LACTATO')]
+        edited_df = st.data_editor(
+            df[['Requisição', 'Exame', 'TA Coleta', 'TAT LAB', 'TAT']],
+            width='stretch',
+            num_rows="dynamic",
+            column_config={
+                "Requisição": st.column_config.NumberColumn("Requisição"),
+                "Exame": st.column_config.TextColumn("Exame"),
+                "TA Coleta": st.column_config.TextColumn("TA Coleta"),
+                "TAT LAB": st.column_config.TextColumn("TAT LAB"),
+                "TAT": st.column_config.TextColumn("TAT"),
+            },
+            key=f"editor_{session_key}"
+        )
 
+        st.session_state[session_key].update(edited_df)
 
-import streamlit as st
+        for col in ['TA Coleta', 'TAT LAB', 'TAT']:
+          edited_df[col] = pd.to_timedelta(edited_df[col], errors="coerce")
+
+        # Calcula métricas
+        df_meta = edited_df[pd.to_timedelta(edited_df['TAT LAB']) < meta]
+        # print(f'{protocolo}: dentro da meta {len(df_meta)} e total {len(edited_df)}')
+        rel_pct = round(len(df_meta) * 100 / len(edited_df), 2)
+
+        def calc_metric(col):
+            t = pd.to_timedelta(edited_df[col]).mean()
+            t_f = str(t.round('1s')).split()[-1]
+            t_dif = str(abs(t - meta).round('1s')).split()[-1]
+            t_arrow = ["up", "red"] if (t - meta) > pd.Timedelta(0) else ["down", "green"]
+            return t_f, t_dif, t_arrow
+
+        tmc_f, tmc_dif, tmc_arrow = calc_metric('TA Coleta')
+        tml_f, tml_dif, tml_arrow = calc_metric('TAT LAB')
+        tat_f, tat_dif, tat_arrow = calc_metric('TAT')
+
+        with metric_container:
+            st.markdown(f":blue-badge[:material/alarm: Meta: {str(meta.round('1s')).split()[-1]}]")
+            
+            a, b = st.columns(2)
+            c, d, e = st.columns(3)
+
+            a.metric(label='Registros', value=len(edited_df), delta='100%', delta_arrow='off', delta_color='blue', border=True)
+            b.metric(label='Dentro da meta', value=len(df_meta), delta=f'{rel_pct}%', delta_arrow='off', delta_color='gray', border=True)
+
+            c.metric(label='Tempo médio de coleta', value=tmc_f, delta=tmc_dif, delta_arrow=tmc_arrow[0], delta_color=tmc_arrow[1], border=True)
+            d.metric(label='Tempo médio de liberação', value=tml_f, delta=tml_dif, delta_arrow=tml_arrow[0], delta_color=tml_arrow[1], border=True)
+            e.metric(label='TAT', value=tat_f, delta=tat_dif, delta_arrow=tat_arrow[0], delta_color=tat_arrow[1], border=True)
+    
+
 
 st.title("Análise TAT")
 
 # st.text("Faça o upload dos dados")
-uploaded_file = st.file_uploader("Escolha o arquivo .CSV", type=["csv", "xlsx"])
+uploaded_file = st.file_uploader("Escolha o arquivo .CSV", type=["csv"], max_upload_size=400)
 
 if uploaded_file is not None:
-  df = pd.read_excel(uploaded_file)
+  data = pd.read_csv(uploaded_file, header=2, usecols=[1,3, 4, 5, 6, 7, 13, 17, 18], dtype={5: manchester_dtype, 6: protocolo_dtype})
 
-  st.write("Dados")
-  st.dataframe(df)
+  # DataFrames
+  sepse_df = data[(data['Protocolo'].str.lower() == 'sepse') & (data['Exame'] == 'LACTATO')]
+  trp_df = data[(data['Protocolo'].str.lower() == 'dor torácica') & (data['Exame'] == 'TRP')]
+  avc_df = data[(data['Protocolo'].str.lower() == 'janela avc') & (data['Exame'] == 'TAP')]
+  # emergencia_df = data[(data['Manchester'].str.lower() == 'emergência') & (data['Exame'] in 'TAP')]
 
-sepse_tab, card_tab, avc_tab, emergencia_tab, murgente_tab, urgente_tab, purgente_tab =st.tabs(['SEPSE', 'DOR TORÁCICA', 'AVC', 'EMERGÊNCIA', 'MUITO URGENTE', 'URGENTE', 'POUCO URGENTE'])
+  # Definição do mês
+  calendario = data['Data'].iloc[0]
+  mes = pd.to_datetime(calendario, dayfirst=True).month
 
-with sepse_tab:
-  sepse_prazo = sepse_df[sepse_df['TAT LAB segundos'] < 2100] # 2100s = 35min
+  st.markdown(f':grey-badge[:material/calendar_month: **{MESES[mes]}**]', width="stretch")
 
-  st.markdown(f'''
-              Tempos de entrega de **lactato** em **protocolo de sepse**.  
-              Total de registros: **{len(sepse_df)}**.  
-              Entregas dentro do prazo (35 min): **{len(sepse_prazo)}**
-              ''')
-  st.data_editor(sepse_df.iloc[:, :5])
+  # Tabs
+  sepse_tab, card_tab, avc_tab, emergencia_tab, murgente_tab, urgente_tab, purgente_tab =st.tabs(['SEPSE', 'DOR TORÁCICA', 'AVC', 'EMERGÊNCIA', 'MUITO URGENTE', 'URGENTE', 'POUCO URGENTE'])
 
-with emergencia_tab:
-  st.data_editor(urgencias_20)
+  mostrar_protocolo_tab(sepse_tab, sepse_df, 'sepse_df', 'Sepse', 'LACTATO', pd.Timedelta(minutes=35))
+  mostrar_protocolo_tab(card_tab, trp_df, 'trp_df', 'Dor torácica', 'TRP', pd.Timedelta(minutes=60))
+  mostrar_protocolo_tab(avc_tab, avc_df, 'avc_df', 'Janela AVC', 'TAP', pd.Timedelta(minutes=45))
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,7 @@
 import pandas as pd
 from pandas.api.types import CategoricalDtype
 
-EXAMES_1 = ["GASO", "GASOV", "HT", "HB", "PLA", "NA", "K", "CAIO"]
-EXAMES_2 = ["H", "TAP", "KPTT", "TRP", "PCRQ", "LACTATO", "AMI"]
-EXAMES_3 = ["AUR", "ALB", "BIL", "CA", "CPK", "CKMB", "CL", "FERRO", "FAL", "LDH", "FOSFORO", "G", "LIPASE", "MAG", "PT", "GGT", "TGO", "TGP", "U", "FIB", "BHCG", "EAS"]
-
-MESES = {
-    1: "Janeiro", 2: "Fevereiro", 3: "Março",
-    4: "Abril", 5: "Maio", 6: "Junho",
-    7: "Julho", 8: "Agosto", 9: "Setembro",
-    10: "Outubro", 11: "Novembro", 12: "Dezembro"
-}
+import streamlit as st
 
 manchester_dtype = CategoricalDtype(
   categories=[
@@ -29,183 +20,110 @@ protocolo_dtype = CategoricalDtype(
   ]
 )
 
-import streamlit as st
+MESES = {
+    1: "Janeiro", 2: "Fevereiro", 3: "Março",
+    4: "Abril", 5: "Maio", 6: "Junho",
+    7: "Julho", 8: "Agosto", 9: "Setembro",
+    10: "Outubro", 11: "Novembro", 12: "Dezembro"
+}
 
+METAS = {
+  'sepse': [35],
+  'dor torácica': [60],
+  'janela avc': [45],
+  'emergência': [20, 45, 80],
+  'muito urgente': [30, 60, 100],
+  'urgente': [50, 70, 120],
+  'pouco urgente': [60, 100, 130],
+  'não urgente': [80, 120, 150]
+}
 
-def metric_data(df, tempo_max):
-    tempo_medio = pd.to_timedelta(sepse_df['TA Coleta']).mean()
+def mostrar_protocolo_tab(tab, df, session_key, protocolo, exame, meta):
+    """
+    Exibe uma aba de protocolo com métricas de tempo.
+    """
 
-    meta = pd.Timedelta(minutes=tempo_max)
+    with tab:
+        st.markdown(f'##### Protocolo de *{protocolo}*: `{exame}`')
 
-    diferenca = tempo_medio - meta
+        if session_key not in st.session_state:
+            st.session_state[session_key] = df.copy()
 
-    return tempo_medio, diferenca
+        metric_container = st.container()
+
+        edited_df = st.data_editor(
+            df[['Requisição', 'Exame', 'TA Coleta', 'TAT LAB', 'TAT']],
+            width='stretch',
+            num_rows="dynamic",
+            column_config={
+                "Requisição": st.column_config.NumberColumn("Requisição"),
+                "Exame": st.column_config.TextColumn("Exame"),
+                "TA Coleta": st.column_config.TextColumn("TA Coleta"),
+                "TAT LAB": st.column_config.TextColumn("TAT LAB"),
+                "TAT": st.column_config.TextColumn("TAT"),
+            },
+            key=f"editor_{session_key}"
+        )
+
+        st.session_state[session_key].update(edited_df)
+
+        for col in ['TA Coleta', 'TAT LAB', 'TAT']:
+          edited_df[col] = pd.to_timedelta(edited_df[col], errors="coerce")
+
+        # Calcula métricas
+        df_meta = edited_df[pd.to_timedelta(edited_df['TAT LAB']) < meta]
+        # print(f'{protocolo}: dentro da meta {len(df_meta)} e total {len(edited_df)}')
+        rel_pct = round(len(df_meta) * 100 / len(edited_df), 2)
+
+        def calc_metric(col):
+            t = pd.to_timedelta(edited_df[col]).mean()
+            t_f = str(t.round('1s')).split()[-1]
+            t_dif = str(abs(t - meta).round('1s')).split()[-1]
+            t_arrow = ["up", "red"] if (t - meta) > pd.Timedelta(0) else ["down", "green"]
+            return t_f, t_dif, t_arrow
+
+        tmc_f, tmc_dif, tmc_arrow = calc_metric('TA Coleta')
+        tml_f, tml_dif, tml_arrow = calc_metric('TAT LAB')
+        tat_f, tat_dif, tat_arrow = calc_metric('TAT')
+
+        with metric_container:
+            st.markdown(f":blue-badge[:material/alarm: Meta: {str(meta.round('1s')).split()[-1]}]")
+            
+            a, b = st.columns(2)
+            c, d, e = st.columns(3)
+
+            a.metric(label='Registros', value=len(edited_df), delta='100%', delta_arrow='off', delta_color='blue', border=True)
+            b.metric(label='Dentro da meta', value=len(df_meta), delta=f'{rel_pct}%', delta_arrow='off', delta_color='gray', border=True)
+
+            c.metric(label='Tempo médio de coleta', value=tmc_f, delta=tmc_dif, delta_arrow=tmc_arrow[0], delta_color=tmc_arrow[1], border=True)
+            d.metric(label='Tempo médio de liberação', value=tml_f, delta=tml_dif, delta_arrow=tml_arrow[0], delta_color=tml_arrow[1], border=True)
+            e.metric(label='TAT', value=tat_f, delta=tat_dif, delta_arrow=tat_arrow[0], delta_color=tat_arrow[1], border=True)
+    
 
 
 st.title("Análise TAT")
 
 # st.text("Faça o upload dos dados")
-uploaded_file = st.file_uploader("Escolha o arquivo .CSV", type=["csv", "xlsx"], max_upload_size=400)
+uploaded_file = st.file_uploader("Escolha o arquivo .CSV", type=["csv"], max_upload_size=400)
 
 if uploaded_file is not None:
   data = pd.read_csv(uploaded_file, header=2, usecols=[1,3, 4, 5, 6, 7, 13, 17, 18], dtype={5: manchester_dtype, 6: protocolo_dtype})
 
-  # Converte tempos em segundos
-  # data['TA Coleta'] = pd.to_timedelta(data.iloc[:, 3]).dt.total_seconds()
-  # data['TA Coleta segundos'] = pd.to_timedelta(data['TA Coleta']).dt.total_seconds()
-  # data['TAT LAB segundos'] = pd.to_timedelta(data['TAT LAB']).dt.total_seconds()
+  # DataFrames
+  sepse_df = data[(data['Protocolo'].str.lower() == 'sepse') & (data['Exame'] == 'LACTATO')]
+  trp_df = data[(data['Protocolo'].str.lower() == 'dor torácica') & (data['Exame'] == 'TRP')]
+  avc_df = data[(data['Protocolo'].str.lower() == 'janela avc') & (data['Exame'] == 'TAP')]
+  # emergencia_df = data[(data['Manchester'].str.lower() == 'emergência') & (data['Exame'] in 'TAP')]
 
-  # print(data)
-
-  # Filtra todas linhas com Manchester == URGENTE
-  # urgencias_df = data[data['Manchester'] == 'URGENTE']
-
-  # print(len(uregencias))
-
-  # urgencias_20 = data[data['TAT LAB segundos'] <= 1200.0]
-  # print(urgencias_20)
-
-  sepse_df = data[(data['Protocolo'] == 'SEPSE') & (data['Exame'] == 'LACTATO')]
-  trp_df = data[(data['Protocolo'] == 'Dor Torácica') & (data['Exame'] == 'TRP')]
-  avc_df = data[(data['Protocolo'] == 'Janela AVC') & (data['Exame'] == 'TAP')]
-  # print(trp_df)
-
+  # Definição do mês
   calendario = data['Data'].iloc[0]
   mes = pd.to_datetime(calendario, dayfirst=True).month
 
-  # st.markdown(f'Mês: **{MESES[mes]}**')
   st.markdown(f':grey-badge[:material/calendar_month: **{MESES[mes]}**]', width="stretch")
 
+  # Tabs
   sepse_tab, card_tab, avc_tab, emergencia_tab, murgente_tab, urgente_tab, purgente_tab =st.tabs(['SEPSE', 'DOR TORÁCICA', 'AVC', 'EMERGÊNCIA', 'MUITO URGENTE', 'URGENTE', 'POUCO URGENTE'])
 
-  with sepse_tab:
-    st.markdown('##### Procolo de *sepse*: `LACTATO`')
-
-    # states
-    if "sepse_df" not in st.session_state:
-      st.session_state.sepse_df = sepse_df.copy()
-
-    metric_container = st.container()
-
-    edited_sepse_df = st.data_editor(
-              sepse_df[['Requisição', 'Exame', 'TA Coleta', 'TAT LAB', 'TAT']],
-              use_container_width=True,
-              num_rows="dynamic",  # permite adicionar/remover linhas
-              column_config={
-                  "Requisição": st.column_config.NumberColumn("Requisição"),
-                  "Exame": st.column_config.TextColumn("Exame"),
-                  "TA Coleta": st.column_config.TextColumn("TA Coleta"),
-                  "TAT LAB": st.column_config.TextColumn("TAT LAB"),
-                  "TAT": st.column_config.TextColumn("TAT"),
-              },
-              key="editor_sepse")
-
-    st.session_state.sepse_df.update(edited_sepse_df)
-
-    # Converte tempos
-    for col in ['TA Coleta', 'TAT LAB', 'TAT']:
-        edited_sepse_df[col] = pd.to_timedelta(edited_sepse_df[col], errors="coerce")
-
-    # Variaveis
-    meta = pd.Timedelta(minutes=35)
-    sepse_prazo = edited_sepse_df[pd.to_timedelta(edited_sepse_df['TAT LAB']) < meta]
-    sepse_rel = round(((len(sepse_prazo) * 100) / len(edited_sepse_df)), 2)
-
-    tmc = pd.to_timedelta(edited_sepse_df['TA Coleta']).mean()
-    tmc_f = str(tmc.round('1s')).split()[-1]
-    tmc_dif = str(abs(tmc - meta).round('1s')).split()[-1]
-    tmc_arrow = ["up", "red"] if (tmc - meta) > pd.Timedelta(0) else ["down", "green"]
-
-    tml = pd.to_timedelta(edited_sepse_df['TAT LAB']).mean()
-    tml_f = str(tml.round('1s')).split()[-1]
-    tml_dif = str(abs(tml - meta).round('1s')).split()[-1]
-    tml_arrow = ["up", "red"] if (tml - meta) > pd.Timedelta(0) else ["down", "green"]
-
-    tat = pd.to_timedelta(edited_sepse_df['TAT']).mean()
-    tat_f = str(tat.round('1s')).split()[-1]
-    tat_dif = str(abs(tat - meta).round('1s')).split()[-1]
-    tat_arrow = ["up", "red"] if (tat - meta) > pd.Timedelta(0) else ["down", "green"]
-
-
-    with metric_container:
-      st.markdown(f":blue-badge[:material/alarm: Meta: {str(meta.round('1s')).split()[-1]}]")
-       
-      a, b = st.columns(2)
-      c, d, e = st.columns(3)
-      
-      a.metric(label='Registros', value=len(edited_sepse_df), delta='100%', delta_arrow='off', delta_color='blue', border=True)
-      b.metric(label='Dentro da meta', value=len(sepse_prazo), delta=f'{sepse_rel}%', delta_arrow='off', delta_color='gray', border=True)
-
-      c.metric(label='Tempo médio de coleta', value=tmc_f, delta=tmc_dif, delta_arrow=tmc_arrow[0], delta_color=tmc_arrow[1], border=True)
-      d.metric(label='Tempo médio de liberação', value=tml_f, delta=tml_dif, delta_arrow=tml_arrow[0], delta_color=tml_arrow[1], border=True)
-      e.metric(label='TAT', value=tat_f, delta=tat_dif, delta_arrow=tat_arrow[0], delta_color=tat_arrow[1], border=True)
-
-  with card_tab:
-    # Variaveis
-    meta = pd.Timedelta(minutes=60)
-    trp_prazo = trp_df[pd.to_timedelta(trp_df['TAT LAB']) < meta]
-    trp_rel = round(((len(trp_prazo) * 100) / len(trp_df)), 2)
-
-    st.markdown(f":blue-badge[:material/alarm: Meta: {str(meta.round('1s')).split()[-1]}]")
-
-    tmc = pd.to_timedelta(trp_df['TA Coleta']).mean()
-    tmc_f = str(tmc.round('1s')).split()[-1]
-    tmc_dif = str(abs(tmc - meta).round('1s')).split()[-1]
-    tmc_arrow = ["up", "red"] if (tmc - meta) > pd.Timedelta(0) else ["down", "green"]
-
-    tml = pd.to_timedelta(trp_df['TAT LAB']).mean()
-    tml_f = str(tml.round('1s')).split()[-1]
-    tml_dif = str(abs(tml - meta).round('1s')).split()[-1]
-    tml_arrow = ["up", "red"] if (tml - meta) > pd.Timedelta(0) else ["down", "green"]
-
-    tat = pd.to_timedelta(trp_df['TAT']).mean()
-    tat_f = str(tat.round('1s')).split()[-1]
-    tat_dif = str(abs(tat - meta).round('1s')).split()[-1]
-    tat_arrow = ["up", "red"] if (tat - meta) > pd.Timedelta(0) else ["down", "green"]
-
-    a, b = st.columns(2)
-    c, d, e = st.columns(3)
-    
-    a.metric(label='Registros', value=len(trp_df), delta='100%', delta_arrow='off', delta_color='blue', border=True)
-    b.metric(label='Dentro da meta', value=len(trp_prazo), delta=f'{trp_rel}%', delta_arrow='off', delta_color='gray', border=True)
-
-    c.metric(label='Tempo médio de coleta', value=tmc_f, delta=tmc_dif, delta_arrow=tmc_arrow[0], delta_color=tmc_arrow[1], border=True)
-    d.metric(label='Tempo médio de liberação', value=tml_f, delta=tml_dif, delta_arrow=tml_arrow[0], delta_color=tml_arrow[1], border=True)
-    e.metric(label='TAT', value=tat_f, delta=tat_dif, delta_arrow=tat_arrow[0], delta_color=tat_arrow[1], border=True)
-
-    st.data_editor(trp_df[['Requisição', 'Exame', 'TA Coleta', 'TAT LAB', 'TAT']])
-
-  with avc_tab:
-    # Variaveis
-    meta = pd.Timedelta(minutes=45)
-    avc_prazo = avc_df[pd.to_timedelta(avc_df['TAT LAB']) < meta]
-    avc_rel = round(((len(avc_prazo) * 100) / len(avc_df)), 2)
-
-    st.markdown(f":blue-badge[:material/alarm: Meta: {str(meta.round('1s')).split()[-1]}]")
-
-    tmc = pd.to_timedelta(avc_df['TA Coleta']).mean()
-    tmc_f = str(tmc.round('1s')).split()[-1]
-    tmc_dif = str(abs(tmc - meta).round('1s')).split()[-1]
-    tmc_arrow = ["up", "red"] if (tmc - meta) > pd.Timedelta(0) else ["down", "green"]
-
-    tml = pd.to_timedelta(avc_df['TAT LAB']).mean()
-    tml_f = str(tml.round('1s')).split()[-1]
-    tml_dif = str(abs(tml - meta).round('1s')).split()[-1]
-    tml_arrow = ["up", "red"] if (tml - meta) > pd.Timedelta(0) else ["down", "green"]
-
-    tat = pd.to_timedelta(avc_df['TAT']).mean()
-    tat_f = str(tat.round('1s')).split()[-1]
-    tat_dif = str(abs(tat - meta).round('1s')).split()[-1]
-    tat_arrow = ["up", "red"] if (tat - meta) > pd.Timedelta(0) else ["down", "green"]
-
-    a, b = st.columns(2)
-    c, d, e = st.columns(3)
-    
-    a.metric(label='Registros', value=len(avc_df), delta='100%', delta_arrow='off', delta_color='blue', border=True)
-    b.metric(label='Dentro da meta', value=len(avc_prazo), delta=f'{avc_rel}%', delta_arrow='off', delta_color='gray', border=True)
-
-    c.metric(label='Tempo médio de coleta', value=tmc_f, delta=tmc_dif, delta_arrow=tmc_arrow[0], delta_color=tmc_arrow[1], border=True)
-    d.metric(label='Tempo médio de liberação', value=tml_f, delta=tml_dif, delta_arrow=tml_arrow[0], delta_color=tml_arrow[1], border=True)
-    e.metric(label='TAT', value=tat_f, delta=tat_dif, delta_arrow=tat_arrow[0], delta_color=tat_arrow[1], border=True)
-
-    st.data_editor(avc_df[['Requisição', 'Exame', 'TA Coleta', 'TAT LAB', 'TAT']])
+  mostrar_protocolo_tab(sepse_tab, sepse_df, 'sepse_df', 'Sepse', 'LACTATO', pd.Timedelta(minutes=35))
+  mostrar_protocolo_tab(card_tab, trp_df, 'trp_df', 'Dor torácica', 'TRP', pd.Timedelta(minutes=60))
+  mostrar_protocolo_tab(avc_tab, avc_df, 'avc_df', 'Janela AVC', 'TAP', pd.Timedelta(minutes=45))
