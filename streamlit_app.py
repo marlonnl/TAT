fdc59af40b52c1205f72e2686ebe3a11bf5763e5
Marlonn Locatelli <marlonn@gmail.com>
2026-03-06 21:54:12 -0300

    Change encoding from 'mbcs' to 'utf-8'

--- streamlit_app.py
+++ streamlit_app.py
@@ -254,7 +254,7 @@ if uploaded_file is not None:
     data = pd.read_csv(
         uploaded_file, header=2, usecols=[1, 3, 4, 5, 6, 7, 13, 17, 18],
         dtype={5: manchester_dtype, 6: protocolo_dtype},
-        encoding='mbcs'
+        encoding='utf-8'
     )
 
     # Filtragens
