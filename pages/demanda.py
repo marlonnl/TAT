d4cef004321a6018506a9390dc054903561fd4fb
Marlonn Locatelli <marlonn@gmail.com>
2026-03-14 00:49:22 -0300

    ✨ added demanda page

---
+++ pages/demanda.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+
+from utils import show_date_badge, get_dataframe, no_file
+
+
+st.title("Análise tempo de coleta")
+
+df = get_dataframe()
+
+if df is not None:
+    show_date_badge()
+    st.write("...")
